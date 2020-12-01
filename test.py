bbc946fb6e0b202774c2b1939b11d7478b01cf75
sarahrosehassan <59797227+sarahrosehassan@users.noreply.github.com>
2020-12-01 16:07:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,17 @@
 import encryption
 
 def testEncryption():
-  # 1.
   key = encryption.readFile('key')
 
-  # 2.
   plainText = encryption.readFile('plainText')
   cipherText = encryption.encrypt(plainText, key)
 
-  # 3.
   cipherText = encryption.writeFile('cipherText', cipherText)
   cipherText = encryption.readFile('cipherText')
 
-  # 4.
   cipherText = encryption.readFile('cipherText')
   decryptedText = encryption.decrypt (cipherText, key)
 
-  # 5.
   decryptedText = encryption.writeFile('plainText1', decryptedText)
   decryptedText = encryption.readFile('plainText1')
   
